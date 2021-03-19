9de643eb50a83aa18351cd3180ae54389f958dc1
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-19 19:47:29 +0100

    ex01 Recursion

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -4,12 +4,12 @@ function myRecursion(arr, n) {
     if(n <= 0){
         return 0;
     } else{
-        return myRecursion(arr, n -1) + arr[n - 1];
+        return myRecursion(arr, n - 1) + arr[n - 1];
     }
 
 }
 
-// Only change code below this line
+// Only change code above this line
 
 console.log(myRecursion([1], 0)); // Change this line
 console.log(myRecursion([1, 2, 3, 4], 2));
